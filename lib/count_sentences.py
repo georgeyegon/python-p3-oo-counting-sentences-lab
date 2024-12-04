fdec2ec1f566861fbf382f37a3d2692473b691c5
George Yegon <georgeyegon67@gmail.com>
2024-12-04 11:23:19 +0300

    initial commit

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -1,4 +1,32 @@
 #!/usr/bin/env python3
 
 class MyString:
-  pass
+    def __init__(self, value=""):
+        self._value = ""
+        self.value = value  # Use the property setter to validate
+
+    @property
+    def value(self):
+        return self._value
+
+    @value.setter
+    def value(self, new_value):
+        if isinstance(new_value, str):
+            self._value = new_value
+        else:
+            print("The value must be a string.")
+
+    def is_sentence(self):
+        return self.value.endswith(".")
+
+    def is_question(self):
+        return self.value.endswith("?")
+
+    def is_exclamation(self):
+        return self.value.endswith("!")
+
+    def count_sentences(self):
+        import re
+        cleaned_value = re.sub(r"[!?]+", ".", self.value)  # Replace ? and ! with .
+        sentences = [s.strip() for s in cleaned_value.split(".") if s.strip()]
+        return len(sentences)
